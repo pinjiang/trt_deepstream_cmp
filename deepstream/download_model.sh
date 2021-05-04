b8db5c3172062ddfe5021a8538ea6da54fff77f3
pinjiang <30993195+pinjiang@users.noreply.github.com>
2021-05-04 10:38:38 +0800

    Update download_model.sh
    Update the model download link

--- deepstream/download_model.sh
+++ deepstream/download_model.sh
@@ -1,1 +1,1 @@
-wget https://drive.google.com/file/d/1Vz3vDJnGlFHxvArkok6L7V3N0e7flRc_/view?usp=sharing -O models/resnet34_3inputs_448x448_20200609.trt
+wget https://drive.google.com/u/0/uc?export=download&confirm=CfOF&id=1Vz3vDJnGlFHxvArkok6L7V3N0e7flRc_ -O models/resnet34_3inputs_448x448_20200609.trt
